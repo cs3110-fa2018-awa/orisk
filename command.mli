99b53051c439299fba4b9827d6df950deae02683
Will Smith <wds68@cornell.edu>
2018-10-23 23:30:10 -0400

    more stuffs

--- command.mli
+++ command.mli
@@ -15,4 +15,6 @@ exception Empty
 (** Raised when a malformed command is encountered. *)
 exception Malformed
 
+(** [parse str] returns the command parsed from [str] - AttackC, ReinforceC, 
+    Quit. *)
 val parse : string -> command
